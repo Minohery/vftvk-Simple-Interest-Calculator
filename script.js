85007a6003143cfe617e1317d519939a4c915e78
Minohery Razafimino <minoheryoni@yahoo.com>
2022-03-31 08:04:20 -0500

    modified index.txt, script.js, style.css

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    /* Validation input. If the value entered by the user in principal
+    is less than or equal to zero, the calculation does not execute */
+    if (principal <= 0)
+        alert("Enter a positive number");
+
+    /*If the value entered by the user for principal is greater than zero,
+     the calculation may be peformed */
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+
+        // Convert the string values into Float
+        var amount = parseFloat(principal) + parseFloat(interest);
+        document.getElementById("result").innerHTML =
+            "If you deposit " + "<mark>" + principal + "</mark>" +
+            ",\<br\>at an interest rate of " + "<mark>" + rate +
+            "%</mark>,\<br\>you will receive an amount of " + "<mark>" + amount +
+            "</mark>" + ",\<br\>in the year " + year
+    }
+}
+
+// Display rate dynamically
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
